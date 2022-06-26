1a07f33711b8b9cfa523e15604f5d6cde63c4a66
hersula <herman.sulaeman@gmail.com>
2022-06-26 19:37:44 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,13 @@
 <?php 
 
-    $host = 'localhost';
+    // $host = 'localhost';
     // $user = 'norbumed_operational';
     // $pass = 'yn8bOL[6MlPZ';
-    $user = 'root';
-    $pass = '';
+    // $user = 'root';
+    // $pass = '';
+    $host = '51.79.150.45';
+    $user = 'hersul';
+    $pass = 'Qaz123wsxa@gtXvsradeon=slivscrossfireX';
     $db_name = 'norbumed_operational_db';
 
     $conn = mysqli_connect($host, $user, $pass, $db_name);
